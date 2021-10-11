517aa3613c6e651c41ef2c6f31a727fee78f9613
Sergei <mr.sergei82@mail.ru>
2021-10-11 20:13:14 +0300

    Сделал compare ещё раз

--- src/Box.java
+++ src/Box.java
@@ -1,28 +1,39 @@
-
+import java.nio.charset.StandardCharsets;
 
 public class Box<T extends Fruit> {
     private T[] obj;
 
-    public Box(T[] obj) {
-        this.obj = obj;
+    public Box(T[] fruitArr) {
+        this.obj = fruitArr;
     }
+    public int F =0;
 
-    public T[] getObj() {
-        return obj;
-    }
-    public void getWeight(T[] obj){
+    public void getWeight(){
+        for (int i=0;i<obj.length;i++){
+            if (obj[i] != null){
+                F++;
+            }
+        }
         if (obj instanceof Apple[]){
-            System.out.println(obj.length*Apple.waight);
-            Main.boxWaight=(obj.length*Apple.waight);
+            System.out.println(F*Apple.waight);
+            Main.boxWaight=(F*Apple.waight);
         }else if (obj instanceof Orange[]){
-            System.out.println(obj.length*Orange.waight);
-            Main.boxWaight=(obj.length*Orange.waight);
+            System.out.println(F*Orange.waight);
+            Main.boxWaight=(F*Orange.waight);
         }
     }
     public boolean compare(Box<?> ob) {
+
+
         if (this == ob) {
             return true;
         }else return false;
+
+    }
+    public void peresipanie(Box<?> ob){
+        for (int i =0; i<F;i++){
+        this.obj[i]=obj[i];
+        }
 
     }
 }
